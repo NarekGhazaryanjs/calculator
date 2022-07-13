36d54b4898d6d1fe47c615c93d31a0c016b65be6
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-13 18:37:05 +0400

    functions

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,34 +1,71 @@
+/* eslint-disable no-eval */
 import Card from "./components/Card/Card";
 import classes from './ui/Global.module.css'
 import './App.css'
 import Button from "./components/Button";
+import { useState } from "react";
 
 const App = () => {
+  const [display,setDisplay] = useState('')
+
+  const AddToDisplay = (event) => {
+    setDisplay(display + event.target.innerText)
+  }
+
+
+  const setResult = () => {
+    setDisplay(eval(display))
+  }
+
+  const deleteLastDigit = () => {
+     let newDisplay = Math.ceil(display) ;
+     newDisplay = newDisplay + '';
+     newDisplay =  newDisplay.substring(0, display.length - 1)
+     setDisplay(newDisplay)
+  }
+
+  const mathPow = () => {
+    setDisplay(display * display)
+  }
+
+  const sqrt = () => {
+    setDisplay( Math.sqrt(Number(display)) )
+  }
+
+  const clearDisplay = () => {
+    setDisplay('')
+  }
+
   return (
     <Card className={classes.container}>
       <Card className={classes.header}>
-          
+          <span>{display}</span> 
       </Card>
+      <Card className={classes.clear}>
+          <Button onClick={clearDisplay}>Clear</Button>
+      </Card>
+         
 
       <Card className={classes.main}>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>/</Button>
-          <Button>*</Button>
-          <Button>+</Button>
-          <Button>-</Button>
-          <Button>0</Button>
-          <Button>delete</Button>
-          <Button>Clear</Button>
-          <Button>sqrt</Button>
-          <Button>pow</Button>
+          <Button onClick={AddToDisplay}>1</Button>
+          <Button onClick={AddToDisplay}>2</Button>
+          <Button onClick={AddToDisplay}>3</Button>
+          <Button onClick={AddToDisplay}>4</Button>
+          <Button onClick={AddToDisplay}>5</Button>
+          <Button onClick={AddToDisplay}>6</Button>
+          <Button onClick={AddToDisplay}>7</Button>
+          <Button onClick={AddToDisplay}>8</Button>
+          <Button onClick={AddToDisplay}>9</Button>
+          <Button onClick={AddToDisplay}>/</Button>
+          <Button onClick={AddToDisplay}>*</Button>
+          <Button onClick={AddToDisplay}>+</Button>
+          <Button onClick={AddToDisplay}>-</Button>
+          <Button onClick={AddToDisplay}>0</Button>
+          <Button onClick={deleteLastDigit}>delete</Button>
+          <Button onClick={mathPow}>pow</Button>
+          <Button onClick={sqrt}>sqrt</Button>
+          <Button onClick={setResult}>=</Button>
+          
       </Card>
     </Card>
   )
